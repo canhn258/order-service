d3fb8c6c7aecb4b9c1b4004a25b13cd6917cf60d
Canh Nguyen Van <canhnvuit@gmail.com>
2024-03-23 19:03:11 +0700

    Define OrderRepository extends a reactive repository providing CRUD operations, specifies the type of managed entity (Order), and its primary key type (Long)

---
+++ src/main/java/com/polarbookshop/orderservice/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.polarbookshop.orderservice.repository;
+
+import com.polarbookshop.orderservice.domain.Order;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
+
+}
