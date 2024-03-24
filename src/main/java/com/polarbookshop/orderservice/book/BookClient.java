104f7cc45ce7afbd4b539d8e614b98f20acfacce
Canh Nguyen Van <canhnvuit@gmail.com>
2024-03-24 17:49:50 +0700

    Define a timeout for WebClient

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Component
 public class BookClient {
     private final WebClient webClient;
@@ -17,6 +19,7 @@ public class BookClient {
                 .get()
                 .uri("/books/{isbn}", isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty());
     }
 }
